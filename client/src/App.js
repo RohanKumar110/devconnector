94e2492a1bbb885e6576f0c748e499cae3dc0738
Rohan Kumar <rohanktalreja110@gmail.com>
2021-08-16 18:26:30 +0500

    add redux store

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,26 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/layout/LandingPage";
 import React, { Fragment } from "react";
 
+// Redux
+import store from "./store";
+import { Provider } from "react-redux";
+
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={LandingPage} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={LandingPage} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
